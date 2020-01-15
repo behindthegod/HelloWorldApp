1e72c3e7d52589098ead4fcdd50ecaea35cd485a
57k <57k@mail.ru>
2020-01-15 16:05:13 +0300

    Initial Commit

---
+++ Ilusa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ilusa
+//
+//  Created by Илья Карлюк on 15.01.2020.
+//  Copyright © 2020 Илья Карлюк. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
